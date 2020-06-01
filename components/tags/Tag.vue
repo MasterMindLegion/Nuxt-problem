a27dd449897cc9a25a918db98a60bc1c27a46552
[mastermindlegion] <[martiin.chalupa@gmail.com]>
2020-06-01 17:03:58 +0200

    Tag fetch done

--- components/tags/Tag.vue
+++ components/tags/Tag.vue
@@ -1,19 +1,58 @@
 <template>
     <div>
-        Im the one tag
-        {{ tagContent }}
+        <h1>{{ tagSlug }}</h1>
+
+        <!-- Content is loading -->
+        <template v-if='$fetchState.pending'>
+            <p>Loading...</p>
+        </template>
+
+        <!-- Content Loaded -->
+        <template v-else>
+            <div v-for='article in articles' class="container">
+                    <articles-block
+                        :article='article'
+                    />
+            </div>
+
+            <div v-if='articles.length' v-observe-visibility='handleScrolledToBottom'></div>
+        </template>
+
     </div>
 </template>
 
 <script>
+    import ArticlesBlock from '@/components/articles/ArticlesBlock.vue'
     export default {
         data() {
             return {
-                tagContent: null,
+                articles: [],
+                tagSlug: null,
+                currentPage: 1,
+                lastPage: 1,
+                articlesMeta: null,
+            }
+        },
+        methods: {
+            handleScrolledToBottom(isVisible) {
+              if (!isVisible) { return }
+              if (this.currentPage >= this.lastPage) { return }
+
+              this.currentPage++
+
+              this.$fetch()
             }
         },
         async fetch() {
-            this.tagContent = await this.$axios.$get(`/tags/${this.$route.params.slug}`)
+            let response = await this.$axios.$get(`/tags/${this.$route.params.slug}?page=${this.currentPage}`)
+            this.articles = this.articles.concat(response.data)
+            this.articlesMeta = response.meta
+            this.lastPage = response.meta.last_page
+
+            this.tagSlug = this.$route.params.slug
+        },
+        components: {
+            ArticlesBlock
         }
     };
 </script>
--- pages/tags/_slug/index.vue
+++ pages/tags/_slug/index.vue
@@ -1,14 +1,11 @@
 <template>
     <div>
-        One tag
         <Tag />
     </div>
 </template>
 
 <script>
-import Tag from '@/components/tags/Tag.vue'
-
-
+    import Tag from '@/components/tags/Tag.vue'
     export default {
         components: {
             Tag
